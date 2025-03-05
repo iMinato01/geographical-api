de4c5c70d0220cd89c0e30c5eb4faac9b4751467
Energy pc2 <gabrielmag559@gmail.com>
2025-03-05 17:00:37 -0600

    Avance endpoints base StateController, reestructuración de módulos

--- src/main/java/gg/controller/StateController.java
+++ src/main/java/gg/controller/StateController.java
@@ -3,28 +3,48 @@ package gg.controller;
 import gg.model.State;
 import gg.service.StateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/state")
 public class StateController {
     @Autowired
     private StateService stateService;
 
-    @GetMapping("/api/states")
-    public List<State> getAllStates(){
-        return stateService.getStates();
+    @GetMapping("/all")
+    public ResponseEntity<List<State>> getAllStates(){
+        List<State> states = stateService.getAllStates();
+        if(!states.isEmpty()){
+            return ResponseEntity.ok().body(states);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
-    @PostMapping("/api/state")
+    @PostMapping("/add")
     public ResponseEntity<State> addState(@RequestBody State state){
-        return stateService.addState(state);
+        State savedState = stateService.addState(state);
+        if(savedState != null && savedState.getId() > 0){
+            return ResponseEntity.ok().body(savedState);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(savedState);
+        }
     }
 
-    @GetMapping("/api/state/{id}")
+    @GetMapping("/id/{id}")
     public ResponseEntity<State> getState(@PathVariable long id){
-        return stateService.getState(id);
+        return stateService.getStateById(id).map(state -> ResponseEntity.ok().body(state))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<State> getState(@PathVariable String name){
+        return stateService.getStateByName(name).map(state -> ResponseEntity.ok().body(state))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
--- src/main/java/gg/service/StateService.java
+++ src/main/java/gg/service/StateService.java
@@ -10,23 +10,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StateService {
     @Autowired
     private StateRepository stateRepository;
 
-    public List<State> getStates(){
+    public List<State> getAllStates(){
         return stateRepository.findAll();
     }
 
-    public ResponseEntity<State> addState(@RequestBody State state){
-        State savedState = stateRepository.save(state);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedState);
+    public State addState(@RequestParam State state){
+        return stateRepository.save(state);
     }
 
-    public ResponseEntity<State> getState(@RequestParam long id){
-        State state = stateRepository.findById(id).orElse(null);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(state);
+    public Optional<State> getStateById(@RequestParam long id){
+        return stateRepository.findById(id);
+    }
+
+    public Optional<State> getStateByName(@RequestParam String name) {
+        return stateRepository.findOneByName(name);
     }
 }
